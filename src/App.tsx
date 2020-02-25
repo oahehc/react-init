1a025cd9b484b48491798f6a8c169cac3b87bc9d
oahehc <oahehc@gmail.com>
2020-02-26 07:14:22 +0800

    init react project with parcel, typescript and styled-component

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const App = () => {
+  return (
+    <div>
+      <Title>Test a little.</Title>
+    </div>
+  );
+};
+
+export default App;
